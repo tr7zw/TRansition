17e094a5b362b6dc97236c24c9680673921064b4
tr7zw <tr7zw@live.de>
2025-04-17 01:08:10 +0200

    Add Alpha values

--- src/main/java/dev/tr7zw/transition/mc/VertexConsumerUtil.java
+++ src/main/java/dev/tr7zw/transition/mc/VertexConsumerUtil.java
@@ -38,16 +38,26 @@ public class VertexConsumerUtil {
 
     public static void addVertex(VertexConsumer cons, Matrix4f matrix4f, float x, float y, float z, float u, float v,
             int overlay, int lightmapUV, float nx, float ny, float nz) {
-        addVertex(cons, matrix4f, x, y, z, u, v, overlay, lightmapUV & 65535, lightmapUV >> 16 & 65535, nx, ny, nz);
+        addVertex(cons, matrix4f, x, y, z, u, v, overlay, lightmapUV, nx, ny, nz, 1f);
+    }
+    
+    public static void addVertex(VertexConsumer cons, Matrix4f matrix4f, float x, float y, float z, float u, float v,
+            int overlay, int lightmapUV, float nx, float ny, float nz, float alpha) {
+        addVertex(cons, matrix4f, x, y, z, u, v, overlay, lightmapUV & 65535, lightmapUV >> 16 & 65535, nx, ny, nz, (int) (alpha * 255));
     }
 
     public static void addVertex(VertexConsumer cons, Matrix4f matrix4f, float x, float y, float z, float u, float v,
             int overlay, int u2, int v2, float nx, float ny, float nz) {
+        addVertex(cons, matrix4f, x, y, z, u, v, overlay, u2, v2, nx, ny, nz, 255);
+    }
+    
+    public static void addVertex(VertexConsumer cons, Matrix4f matrix4f, float x, float y, float z, float u, float v,
+            int overlay, int u2, int v2, float nx, float ny, float nz, int alpha) {
         //#if MC >= 12100
-        cons.addVertex(matrix4f, x, y, z).setColor(255, 255, 255, 255).setUv(u, v).setUv2(u2, v2).setOverlay(overlay)
+        cons.addVertex(matrix4f, x, y, z).setColor(255, 255, 255, alpha).setUv(u, v).setUv2(u2, v2).setOverlay(overlay)
                 .setNormal(nx, ny, nz);
         //#else
-        //$$ cons.vertex(matrix4f, x, y, z).color(1f, 1f, 1f, 1f).uv(u, v).overlayCoords(overlay).uv2(u2, v2).normal(nx, ny, nz)
+        //$$ cons.vertex(matrix4f, x, y, z).color(1f, 1f, 1f, alpha / 255f).uv(u, v).overlayCoords(overlay).uv2(u2, v2).normal(nx, ny, nz)
         //$$ .endVertex();
         //#endif
     }
