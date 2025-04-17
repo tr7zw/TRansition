30ae76ce48a2af9b62370391fd6165c70f5fe9a9
tr7zw <tr7zw@live.de>
2025-04-17 14:03:48 +0200

    Switch to 1.21.6 snapshots as base

--- src/main/java/dev/tr7zw/transition/mc/VertexConsumerUtil.java
+++ src/main/java/dev/tr7zw/transition/mc/VertexConsumerUtil.java
@@ -40,17 +40,18 @@ public class VertexConsumerUtil {
             int overlay, int lightmapUV, float nx, float ny, float nz) {
         addVertex(cons, matrix4f, x, y, z, u, v, overlay, lightmapUV, nx, ny, nz, 1f);
     }
-    
+
     public static void addVertex(VertexConsumer cons, Matrix4f matrix4f, float x, float y, float z, float u, float v,
             int overlay, int lightmapUV, float nx, float ny, float nz, float alpha) {
-        addVertex(cons, matrix4f, x, y, z, u, v, overlay, lightmapUV & 65535, lightmapUV >> 16 & 65535, nx, ny, nz, (int) (alpha * 255));
+        addVertex(cons, matrix4f, x, y, z, u, v, overlay, lightmapUV & 65535, lightmapUV >> 16 & 65535, nx, ny, nz,
+                (int) (alpha * 255));
     }
 
     public static void addVertex(VertexConsumer cons, Matrix4f matrix4f, float x, float y, float z, float u, float v,
             int overlay, int u2, int v2, float nx, float ny, float nz) {
         addVertex(cons, matrix4f, x, y, z, u, v, overlay, u2, v2, nx, ny, nz, 255);
     }
-    
+
     public static void addVertex(VertexConsumer cons, Matrix4f matrix4f, float x, float y, float z, float u, float v,
             int overlay, int u2, int v2, float nx, float ny, float nz, int alpha) {
         //#if MC >= 12100
