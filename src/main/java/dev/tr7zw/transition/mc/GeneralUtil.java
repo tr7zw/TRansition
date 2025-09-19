f46050922d4bdf4554f9a1fd22d46cd6e41fdf7d
tr7zw <tr7zw@live.de>
2025-09-19 15:49:37 +0200

    createKeyMapping Util

--- src/main/java/dev/tr7zw/transition/mc/GeneralUtil.java
+++ src/main/java/dev/tr7zw/transition/mc/GeneralUtil.java
@@ -1,6 +1,9 @@
 package dev.tr7zw.transition.mc;
 
+import java.util.Map;
+
 import lombok.experimental.UtilityClass;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.ResourceLocation;
@@ -10,6 +13,10 @@ import net.minecraft.world.level.Level;
 @UtilityClass
 public class GeneralUtil {
 
+    //#if MC >= 12109
+    private static final Map<String, net.minecraft.client.KeyMapping.Category> categoryCache = new java.util.HashMap<>();
+    //#endif
+    
     public static ResourceLocation getResourceLocation(String namespace, String path) {
         //#if MC >= 12100
         return ResourceLocation.fromNamespaceAndPath(namespace, path);
@@ -26,6 +33,14 @@ public class GeneralUtil {
         //#endif
     }
 
+    public static KeyMapping createKeyMapping(String keyName, int defaultKey, String category) {
+        //#if MC >= 12109
+        return new KeyMapping(keyName, defaultKey, categoryCache.computeIfAbsent(category, c -> new net.minecraft.client.KeyMapping.Category(getResourceLocation(c))));
+        //#else
+        //$$ return new KeyMapping(keyName, defaultKey, category);
+        //#endif
+    }
+
     public static LocalPlayer getPlayer() {
         return Minecraft.getInstance().player;
     }
@@ -37,7 +52,7 @@ public class GeneralUtil {
         //$$ return Minecraft.getInstance().cameraEntity;
         //#endif
     }
-    
+
     public static Level getWorld() {
         return Minecraft.getInstance().level;
     }
