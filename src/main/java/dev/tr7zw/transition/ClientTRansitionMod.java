a2477367cac822cafecd45d30a4ff247c1ef33ed
tr7zw <tr7zw@live.de>
2026-03-19 22:24:05 +0100

    Add more client util logic

--- src/main/java/dev/tr7zw/transition/ClientTRansitionMod.java
+++ src/main/java/dev/tr7zw/transition/ClientTRansitionMod.java
@@ -1,15 +1,17 @@
 package dev.tr7zw.transition;
 
 import dev.tr7zw.transition.config.*;
+import dev.tr7zw.transition.manager.*;
 import dev.tr7zw.transition.mc.*;
 import dev.tr7zw.transition.sentry.*;
 import net.minecraft.client.*;
 import org.apache.logging.log4j.*;
+import java.util.*;
 
-import dev.tr7zw.transition.manager.ConfigScreenManager;
 //? if fabric {
 
 import net.fabricmc.api.ClientModInitializer;
+
 //? }
 
 public class ClientTRansitionMod
@@ -30,7 +32,21 @@ public class ClientTRansitionMod
     public void onInitializeClient() {
 
         SentryInstance.initialize(new SentryDataProviderImpl());
-
+        CodeManager.getInstance().registerCode(new String(Base64.getDecoder().decode("dXVkZGxybHJiYQ==")), () -> {
+            ClientUtil.sendChatMessage(ComponentProvider.literal(new String(Base64.getDecoder().decode(
+                    "WW91IGZvdW5kIHRoZSBzZWNyZXQgY29kZSEgSXQgZGlkbid0IGRvIGFueXRoaW5nLCBidXQgeW91IGZvdW5kIGl0IQ=="))));
+        });
+        CodeManager.getInstance().registerCode(new String(Base64.getDecoder().decode("aSBhbSBldm9uIGduYXNoYmxhZGU=")),
+                () -> {
+                    Minecraft.getInstance().options.advancedItemTooltips = true;
+                    ClientUtil.sendChatMessage(ComponentProvider
+                            .literal(new String(Base64.getDecoder().decode("SSB3YXMgdGhlIGZhdm9yaXRlLg=="))));
+                });
+        CodeManager.getInstance().registerCode(new String(Base64.getDecoder().decode("bWFyY28h")), () -> {
+            ClientUtil.sendToast(
+                    ComponentProvider.literal(new String(Base64.getDecoder().decode("U29tZSB2b2ljZS4uLg=="))),
+                    ComponentProvider.literal(new String(Base64.getDecoder().decode("UG9sbyE="))));
+        });
     }
 
     public void onInitialize() {
--- src/main/java/dev/tr7zw/transition/mc/ClientUtil.java
+++ src/main/java/dev/tr7zw/transition/mc/ClientUtil.java
@@ -1,7 +1,9 @@
 package dev.tr7zw.transition.mc;
 
 import lombok.experimental.UtilityClass;
+import net.minecraft.*;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.toasts.*;
 import net.minecraft.network.chat.Component;
 
 @UtilityClass
@@ -10,8 +12,19 @@ public class ClientUtil {
     private final static Minecraft MC = Minecraft.getInstance();
 
     public static void sendChatMessage(Component message) {
+        // spotless:off
         //$ send_chat_message
         MC.getChatListener().handleSystemMessage(message, false);
+        // spotless:on
+    }
+
+    public static void sendActionBarMessage(Component message) {
+        MC.gui.setOverlayMessage(message, false);
+    }
+
+    public static void sendToast(Component titel, Component message) {
+        //$ send_toast
+        MC.getToastManager().addToast(new SystemToast(SystemToast.SystemToastId.PERIODIC_NOTIFICATION, titel, message));
     }
 
 }
